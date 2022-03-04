cb4ca0f7985e081eaf80534e7f7fcdba8b6c3fdb
vjpenafiel <100840761+vjpenafiel@users.noreply.github.com>
2022-03-04 17:18:37 +0800

    Add files via upload

---
+++ Lecture1/Reading-Assignment/ch3_ex1.c
@@ -0,0 +1,22 @@
+/****************************************
+* CMSC 21-1: Reading Assingment 1 - ex1 *
+* Student: Victor Antonino J. Peñafiel  *
+****************************************/
+
+//preprocessor directive
+#include <stdio.h>
+
+//main function
+int main(void){
+	int i, j;		//declaraton of int variables
+	float x, y;		//declaration of float variables
+
+	//assigning values to the variables
+	i = 10;
+	j = 20;
+	x = 43.2892f;
+	y = 5527.0f;
+
+	//print statement to show equivalent values of the said variables
+	printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);
+}
