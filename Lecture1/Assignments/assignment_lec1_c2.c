49e053192d2915de12610778be43f6c3dc8033ce
vjpenafiel <100840761+vjpenafiel@users.noreply.github.com>
2022-03-08 14:56:31 +0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+
+	int value1 = 87, value2 = 15, valueFinal;
+
+	valueFinal = value1-value2;
+
+	printf("The result of 87 subtracted by 15 is %i", valueFinal);
+
+}
