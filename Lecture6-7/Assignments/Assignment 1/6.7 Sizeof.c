6fd3cecd7b671310da3d8539cf8bd18232dce9c9
vjpenafiel <100840761+vjpenafiel@users.noreply.github.com>
2022-05-23 00:05:05 +0800

    Add files via upload

---
+++ Lecture6-7/Assignments/Assignment 1/6.7 Sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("Size of char data type: %u\n", sizeof (char));
+	printf("size of int data type: %u\n", sizeof (int));
+	printf("Size of float data type: %u\n", sizeof (float));
+	printf("size of double data type: %u\n", sizeof(double));
+	return 0;
+}
---
+++ Lecture6-7/Assignments/Assignment 1/6.8 Sizeof - Total Size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void) {
+	int number[16];
+	
+	size_t n = sizeof(number)/sizeof (number[0]);
+	printf("Total elements the array can hold is: %d\n", n);
+
+	return 0;
+}
